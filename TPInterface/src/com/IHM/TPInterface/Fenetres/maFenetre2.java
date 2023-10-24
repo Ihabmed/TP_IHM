d79352583badac1139b14d5f684682db25c5cab3
Ihabmed <94643335+Ihabmed@users.noreply.github.com>
2023-10-24 21:53:46 +0100

    Update maFenetre2.java

--- TPInterface/src/com/IHM/TPInterface/Fenetres/maFenetre2.java
+++ TPInterface/src/com/IHM/TPInterface/Fenetres/maFenetre2.java
@@ -1,11 +1,48 @@
 package com.IHM.TPInterface.Fenetres;
 
+import javax.swing.*;
+//import java.awt.BorderLayout;
+
 public class maFenetre2 extends javax.swing.JFrame {
-	maFenetre2 ( ){
+	public maFenetre2 ( ){
+		 
+		 // TODO Initiate a window
 		 setTitle("ma premiere fenetre avec swing");
 		 setSize(300,400);
 		 setDefaultCloseOperation(EXIT_ON_CLOSE);
 		 setResizable(false);
 		 setLocationRelativeTo( null );		
+		 
+		 // TODO Initiate a new items
+		 // BorderLayout layout = new BorderLayout();
+		 JPanel container = new JPanel();
+		 BoxLayout layout = new BoxLayout(container,BoxLayout.PAGE_AXIS /*BoxLayout.LINE_AXIS*/);
+		 JButton myButton = new JButton("my first button!");
+		 JButton myButton2 = new JButton("my second button!");
+		 JTextField myText = new JTextField("Hello there SWING!");
+		 JComboBox myCombo = new JComboBox();
+		 JLabel myLabel = new JLabel("My Label With SWING!");
+		 
+		 // TODO add combo box items
+		 myCombo.addItem("IHM");
+		 myCombo.addItem("SE");
+		 myCombo.addItem("RCD");
+		 myCombo.addItem("BDD-A");
+		 
+		 // TODO set container layout
+		 container.setLayout(layout);
+		 
+		 // TODO add items to the container
+		 container.add(myButton /*layout.WEST*/);
+		 container.add(myButton2 /*layout.EAST*/);
+		 container.add(myText /*layout.NORTH*/);
+		 container.add(myCombo /*layout.SOUTH*/);
+		 container.add(myLabel /*layout.CENTER*/);
+		 
+		 // TODO set the content to container
+		 setContentPane(container);
+		 
+		 // TODO add pack to adjust items
+		 pack();
 	}
 }
